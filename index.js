94a5b13d0d0aedeaa4b8340bf7a045a88546879d
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-11-11 10:54:11 +0200

    clean code

--- index.js
+++ index.js
@@ -37,12 +37,3 @@ function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(argv);
-
-// contactsFunc.listContacts().then(console.log);
-// contactsFunc.getContactById(5).then(console.log);
-// contactsFunc.removeContact(2);
-// contactsFunc.addContact(
-//   "Allen Raymond",
-//   "nulla.ante@vestibul.co.uk",
-//   "(992) 914-3792"
-// );
